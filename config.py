2121366205f1e6d72e0d4b9be026a327b49d535f
Svistoflag <119766684+Svistoflag@users.noreply.github.com>
2025-04-07 15:40:49 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 YANDEX_API_KEY = "cea16386-8689-4105-971a-692907582115"
+DADATA_API_KEY = "26ccdbf16ac82b37ae82dfc43e62b7d72a198724"
+MAPBOX_API_KEY = "your_mapbox_access_token"
 
 # Настройки маршрута
 DEFAULT_TRAVEL_TIME_LIMIT_HOURS = 8
@@ -7,10 +9,8 @@ STOP_TIME_MINUTES = 15
 USE_YANDEX_GEOCODER = True
 YANDEX_GEOCODER_ENDPOINT = "https://geocode-maps.yandex.ru/1.x/"
 
-USE_YANDEX_ROUTER = True
-YANDEX_ROUTER_ENDPOINT = "https://api.routing.yandex.net/v2/route"
+USE_MAPBOX_ROUTER = True
+MAPBOX_ROUTER_ENDPOINT = "https://api.mapbox.com/directions/v5/mapbox/driving"
 
 USE_OSRM_BACKUP = True
 OSRM_SERVER_URL = "http://router.project-osrm.org"
-
-DADATA_API_KEY = "26ccdbf16ac82b37ae82dfc43e62b7d72a198724"
